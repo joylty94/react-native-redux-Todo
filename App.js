1910f81456d32e4ea7bfdd3de6ab136acfd252a7
joylty94 <wowmen1757@gmail.com>
2018-09-11 19:58:08 +0900

    self 에러 수정

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import TodoApp from './src/TodoApp'
 import store from './src/store'
 import { Provider } from 'react-redux'
+
+if (typeof global.self === "undefined") global.self = global; // react-native는 원격에서 self 접근할 수 없어, global에 설정해 준다.
+
 export default class App extends React.Component {
   render() {
     return (
