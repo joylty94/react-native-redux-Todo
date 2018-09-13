8f9667b7bbff781138c33a0029b6d125a5f791d3
joylty94 <wowmen1757@gmail.com>
2018-09-13 20:10:27 +0900

    connect 추가

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 import {
   View,
-  Text,
   StyleSheet,
   TextInput,
   TouchableOpacity
 } from "react-native";
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons';
+import { connect } from 'react-redux';
+import { addTodo } from '../actions';
 class AddTodo extends Component {
+
+  state = {
+    text: ''
+  }
+
+  addTodo = (text) => {
+    //redux store
+    this.props.dispatch(addTodo(text))
+    this.setState({ text: '' })
+  }
+
   render() {
     return (
       <View style={{ flexDirection: 'row', marginHorizontal: 20 }}>
         <TextInput
-
-          placeholder="입력"
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+          placeholder="추 가"
           style={{ borderWidth: 1, borderColor: '#f2f2e1', backgroundColor: '#eaeaea', height: 50, flex: 1, padding: 5 }}
         />
-        <TouchableOpacity onPress={() => alert('added todo')}>
+        <TouchableOpacity onPress={() => this.addTodo(this.state.text)}>
           <View style={{ height: 50, backgroundColor: '#eaeaea', alignItems: 'center', justifyContent: 'center' }}>
             <Ionicons name="md-add" size={30} style={{ color: '#de9595', padding: 10 }} />
           </View>
@@ -25,7 +38,7 @@ class AddTodo extends Component {
     );
   }
 }
-export default AddTodo;
+export default connect()(AddTodo);
 
 const styles = StyleSheet.create({
   container: {
